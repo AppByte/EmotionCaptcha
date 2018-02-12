13c06e4bd109e369e5c116697c757670377356af
Daniel Strebinger <daniel@strebinger.at>
2018-02-12 18:10:37 +0100

    Initial commit

---
+++ iOS/EmotionCaptcha/EmotionCaptcha/EmotionCaptcha.h
@@ -0,0 +1,13 @@
+//
+//  EmotionCaptcha.h
+//  EmotionCaptcha
+//
+//  Created by Daniel Strebinger on 12.02.18.
+//  Copyright © 2018 Daniel Strebinger. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EmotionCaptcha : NSObject
+
+@end
---
+++ iOS/EmotionCaptchaClient/EmotionCaptchaClient/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  EmotionCaptchaClient
+//
+//  Created by Daniel Strebinger on 12.02.18.
+//  Copyright © 2018 Daniel Strebinger. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
